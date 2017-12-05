21c88f2c89462398394f97503bcec143ef14788e
star_xlliu <star_xlliu@vipabc.com>
2017-12-05 18:03:43 +0800

    ss

--- test.py
+++ test.py
@@ -9,32 +9,32 @@
 # else :
 #     print count, "is not less than 5"
 
-# import random
-# import sys
-# import time
+import random
+import sys
+import time
 
-# result = []
-# while True :
-#     result.append(int(random.uniform(1, 7)))
-#     result.append(int(random.uniform(1, 7)))
-#     result.append(int(random.uniform(1, 7)))
-#     print result
-#     count = 0
-#     index = 2
-#     pointStr = ""
-#     while index >= 0:
-#         currPoint = result[index]
-#         count += currPoint
-#         index -= 1
-#         pointStr += " "
-#         pointStr += str(currPoint)
-#     if count <= 11:
-#         sys.stdout.write(pointStr + " -> " + "小" + "\n")
-#         time.sleep(1)
-#     else:
-#         sys.stdout.write(pointStr + " -> " + "大" + "\n")
-#         time.sleep(1)
-#     result = []
+result = []
+while True :
+    result.append(int(random.uniform(1, 7)))
+    result.append(int(random.uniform(1, 7)))
+    result.append(int(random.uniform(1, 7)))
+    print result
+    count = 0
+    index = 2
+    pointStr = ""
+    while index >= 0:
+        currPoint = result[index]
+        count += currPoint
+        index -= 1
+        pointStr += " "
+        pointStr += str(currPoint)
+    if count <= 11:
+        sys.stdout.write(pointStr + " -> " + "小" + "\n")
+        time.sleep(1)
+    else:
+        sys.stdout.write(pointStr + " -> " + "大" + "\n")
+        time.sleep(1)
+    result = []
 
 # class Employee:
 #     '所有员工的基类'
