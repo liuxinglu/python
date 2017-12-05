f879fbd8b479e12dee7fcb8f3b05d38f3c13141e
star_xlliu <star_xlliu@vipabc.com>
2017-12-05 18:05:48 +0800

    a

--- test.py
+++ test.py
@@ -1,13 +1,13 @@
 #!/usr/bin/python
 # -*- coding: UTF-8 -*-
-# print "你好";
-# raw_input("\n\nPress the Enter key to exit");
-# count = 0
-# while count < 5 :
-#     print count, "is less than 5"
-#     count = count + 1
-# else :
-#     print count, "is not less than 5"
+print "你好";
+raw_input("\n\nPress the Enter key to exit");
+count = 0
+while count < 5 :
+    print count, "is less than 5"
+    count = count + 1
+else :
+    print count, "is not less than 5"
 
 import random
 import sys
@@ -36,78 +36,57 @@ while True :
         time.sleep(1)
     result = []
 
-# class Employee:
-#     '所有员工的基类'
-#     empCount = 0
+class Employee:
+    '所有员工的基类'
+    empCount = 0
 
-#     def __init__(self, name, salary):
-#         self.name = name
-#         self.salary = salary
-#         Employee.empCount += 1
+    def __init__(self, name, salary):
+        self.name = name
+        self.salary = salary
+        Employee.empCount += 1
 
-#     def displayCount(self):
-#         print "Total Employee %d" % Employee.empCount
+    def displayCount(self):
+        print "Total Employee %d" % Employee.empCount
 
-#     def displayEmployee(self):
-#         print "Name :", self.name, ", Salary :", self.salary
+    def displayEmployee(self):
+        print "Name :", self.name, ", Salary :", self.salary
         
         
-# emp1 = Employee("Zara", 2000)
-# emp2 = Employee("Mani", 5000)
-# emp1.displayCount()
+emp1 = Employee("Zara", 2000)
+emp2 = Employee("Mani", 5000)
+emp1.displayCount()
 
-# import re
-# line = "Cats are smarter than dogs";
+import re
+line = "Cats are smarter than dogs";
 
-# matchObj = re.match(r'dog', line, re.M|re.I)
-# if matchObj:
-#     print "match --> matchObj.group() : ", matchObj.group()
-# else:
-#     print "No match~"
+matchObj = re.match(r'dog', line, re.M|re.I)
+if matchObj:
+    print "match --> matchObj.group() : ", matchObj.group()
+else:
+    print "No match~"
 
-# matchObj = re.search(r'dog', line, re.M|re.I)
-# if matchObj:
-#     print "search --> matchObj.group():", matchObj.group()
-# else:
-#     print "No match~"
+matchObj = re.search(r'dog', line, re.M|re.I)
+if matchObj:
+    print "search --> matchObj.group():", matchObj.group()
+else:
+    print "No match~"
 
-# def double(matched):
-#     value = int(matched.group('values'))
-#     return str(value * 2)
+def double(matched):
+    value = int(matched.group('values'))
+    return str(value * 2)
 
-# s = 'A23G4GHD567'
-# print(re.sub('(?P<values>\d+)', double, s))
+s = 'A23G4GHD567'
+print(re.sub('(?P<values>\d+)', double, s))
 
-# from Tkinter import *
-# root = Tk()
-
-# li = ['C', 'python', 'php', 'html', 'typeScript', 'SQL', 'java']
-# movie = ['CSS', 'jQ', 'exml']
-# listb = Listbox(root)
-# listb2 = Listbox(root)
-# btn = Button(root)
-# canvas = Canvas(root)
-# checkBtn = CHECKBUTTON(root)
-# entry = Entry(root)
-# frame = Frame(root)
-# label = Label(root)
-# menuBtn = Menubutton(root)
-# menu = Menu(root)
-# msg = Message(root)
-# radio = Radiobutton(root)
-# scale = Scale(root)
-# scrollbar = Scrollbar(root)
-# text = Text(root)
-# toplvl = Toplevel(root)
-# spinbox = Spinbox(root)
+li = ['C', 'python', 'php', 'html', 'typeScript', 'SQL', 'java']
+movie = ['CSS', 'jQ', 'exml']
 
 
-# for item in li:
-#     listb.insert(0, item)
+for item in li:
+    listb.insert(0, item)
 
-# for item in movie:
-#     listb2.insert(0, item)
+for item in movie:
+    listb2.insert(0, item)
 
-# listb.pack()
-# listb2.pack()
-# root.mainloop()
+listb.pack()
+listb2.pack()
