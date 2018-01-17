05e21f4131d599f5cee2a4b0ab3e4ab80e423c85
star_xlliu <star_xlliu@vipabc.com>
2018-01-17 15:33:30 +0800

    excel转json json转excel

--- testFile.py
+++ testFile.py
@@ -51,29 +51,68 @@
 #     file.write(a)
 #     file.close()
 
+import os
 import xlrd
+import openpyxl
 import json
 import codecs
 from collections import OrderedDict
 
-def xlsx2json_2():
-    wb = xlrd.open_workbook('/Users/star_xlliu/Documents/test2.xlsx')
-    shArr = wb.sheet_names()
+def xlsx2json_2(args):
+    wb = xlrd.open_workbook(args)
+    shArr = wb.sheet_names() #获取所有sheet名
     for shname in shArr:
         convert_list = []
-        sh = wb.sheet_by_name(shname)
-        title = sh.row_values(1)
-        for rownum in range(2, sh.nrows):
-            rowvalue = sh.row_values(rownum)
+        sh = wb.sheet_by_name(shname) #通过sheet名获得对应sheet内容
+        title = sh.row_values(1)  #获取第二行的内容作为title
+        for rownum in range(2, sh.nrows): #从第三行开始就是数据了
+            rowvalue = sh.row_values(rownum) #每次拿一行数据
             single = OrderedDict()
-            for colnum in range(1, len(rowvalue)):
+            for colnum in range(1, len(rowvalue)): #将这行数据按列读取放入字典中
                 single[title[colnum]] = rowvalue[colnum]
-            convert_list.append(single)
+            convert_list.append(single) #读完之后将字段放入列表中
 
         j = json.dumps(convert_list, ensure_ascii=False)
 
-        with codecs.open('/Users/star_xlliu/Documents/' + shname + '.json', "w", "utf-8") as f:
+        with codecs.open('/Users/star_xlliu/Documents/json/' + shname + '.json', "w", "utf-8") as f:
             f.write(j)
             f.close()
 
-xlsx2json_2()
+# xlsx2json_2('/Users/star_xlliu/Documents/test2.xlsx')
+
+def readFromJson(path):
+    with open(path, 'r') as f:
+        jsonData = json.load(f)
+    return jsonData
+
+def jsonpath2xlsx(path):
+    filePaths = os.listdir(path)
+    sheetArr = []
+    excel = openpyxl.Workbook()
+    for item in filePaths:
+        sheet = excel.create_sheet(unicode(item, "utf-8")[-9: -5], index=0)
+        sheetArr.append(sheet)
+
+    index = 0
+    for item in filePaths:
+        data = readFromJson(path + item)
+        length = len(data)
+        i = 0
+        j = 0
+        line0 = data[0]
+        for k, v in line0.items():
+            j += 1
+            sheetArr[index].cell(row=1, column=j, value=k)
+        
+        while i < length:
+            eachLine = data[i]
+            j = 0
+            i += 1;
+            for v in eachLine.values():
+                j += 1
+                sheetArr[index].cell(row=i + 1, column=j, value=v)
+        index += 1
+    excel.save("/Users/star_xlliu/Documents/test3.xlsx")
+
+
+jsonpath2xlsx('/Users/star_xlliu/Documents/json/');
